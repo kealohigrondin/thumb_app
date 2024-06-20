e32f3cc72d8357e6d3ca350bb9cc1c5e116e402a
Kealohi Grondin <kealohi.grondin@gmail.com>
2024-06-20 10:31:26 -0700

    Adding firendlist, vehiclelist widgets

---
+++ lib/components/profile_page/friend_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FriendList extends StatefulWidget {
+  const FriendList({super.key});
+
+  @override
+  State<FriendList> createState() => _FriendListState();
+}
+
+class _FriendListState extends State<FriendList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Friends'));
+  }
+}
---
+++ lib/components/profile_page/vehicle_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VehicleList extends StatefulWidget {
+  const VehicleList({super.key});
+
+  @override
+  State<VehicleList> createState() => _VehicleListState();
+}
+
+class _VehicleListState extends State<VehicleList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Vehicles'));
+  }
+}
